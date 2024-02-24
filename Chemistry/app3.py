ee13c60bc0e9e870d9deb736c99c0b1ab644d964
ValterGames <68495491+ValterGames-Coder@users.noreply.github.com>
2024-02-24 12:46:59 +0300

    changes

--- Chemistry/app3.py
+++ Chemistry/app3.py
@@ -1,11 +1,10 @@
-from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5 import QtWidgets
 from PyQt5.QtWidgets import QMessageBox
 import sys
 from window1 import Ui_MainMenu
 from window2 import Ui_Window1
 from window3 import Ui_Window2
 from window4 import Ui_Window3
-
 from chemlib import Compound
 import mendeleev
 
@@ -16,11 +15,11 @@ main = Ui_MainMenu()
 main.setupUi(MainMenu)
 MainMenu.show()
 
+
 def open_window1():
-    global Window1
     Window1 = QtWidgets.QDialog()
-    ui = Ui_Window1()
-    ui.setupUi(Window1)
+    ui = Ui_Window2()
+    ui.setupUi(Window2)
     MainMenu.close()
     Window1.show()
 
@@ -41,7 +40,7 @@ def open_window1():
             error = QMessageBox()
             error.setWindowTitle('Ошибка')
             error.setIcon(QMessageBox.Warning)
-            error.setText('Неправильно введина формула')
+            error.setText('Неправильно введена формула')
 
             error.exec_()
 
@@ -75,7 +74,7 @@ def open_window2():
             error = QMessageBox()
             error.setWindowTitle('Ошибка')
             error.setIcon(QMessageBox.Warning)
-            error.setText('Неправильно введина формула')
+            error.setText('Неправильно введена формула')
 
             error.exec_()
 
@@ -86,7 +85,6 @@ def open_window2():
 
 
 def open_window3():
-    global Window3
     Window3 = QtWidgets.QDialog()
     ui = Ui_Window3()
     ui.setupUi(Window3)
@@ -112,7 +110,7 @@ def open_window3():
             error = QMessageBox()
             error.setWindowTitle('Ошибка')
             error.setIcon(QMessageBox.Warning)
-            error.setText('Неправильно введина формула')
+            error.setText('Неправильно введена формула')
 
             error.exec_()
 
